ebf3e063475ead5f51a1b1f61b80e28d2c238501
Aleksandar Dimitrov <git@aleks.bg>
2022-04-26 22:30:27 +0200

    Initial commit

---
+++ src/Test.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Component } from "./Test";
+
+export default {
+  title: "test",
+  component: Component,
+};
+
+export const Test = () => <Component />;
---
+++ src/Test.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export const Component: React.FC = () => {
+  return <span>I am a test</span>;
+};
